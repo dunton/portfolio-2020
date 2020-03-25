b805a71a37a77af96f04e094b3364210de331f3c
Ryan Dunton <ryan.dunton@equinox.com>
2020-03-25 15:23:00 -0400

    bg cover

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
 class Header extends Component {
   render() {
@@ -27,7 +27,7 @@ const HeaderContainer = styled.div`
     height: 50vh;
   }
   background-image: url(${props => props.backgroundImage});
-
+  background-size: cover;
   .content {
     color: white;
     display: flex;
